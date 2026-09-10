408eb30fe90069066bf21b74d444849f5b545d77
Gisadaya <gisseldiaz176@gmail.com>
2026-09-10 14:02:41 -0600

    Agregando código SQL sobre la seguridad de la BD

--- database/04_seguridad.sql
+++ database/04_seguridad.sql
@@ -1,1 +1,35 @@
+-- SEGURIDAD DE LA BASE DE DATOS
 
+___________________________________
+-- CREACIÓN DEL USUARIO DE TRABAJO
+___________________________________
+
+CREATE USER "Usuario_A"
+WITH PASSWORD '';
+
+
+_________________________________
+
+-- ASIGNACIÓN DE PRIVILEGIOS
+________________________________
+
+GRANT CONNECT
+ON DATABASE "AgroSmart_BD"
+TO "Usuario_A";
+
+GRANT USAGE
+ON SCHEMA public
+TO "Usuario_A";
+
+GRANT SELECT, INSERT, UPDATE
+ON ALL TABLES IN SCHEMA public
+TO "Usuario_A";
+
+_______________________________
+
+--  REVOCACIÓN DE PRIVILEGIO
+_______________________________
+
+REVOKE UPDATE
+ON ALL TABLES IN SCHEMA public
+FROM "Usuario_A";
