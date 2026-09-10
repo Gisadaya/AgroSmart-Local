60f59f95f080cb47a54ccaf46cd36366eccccd29
Gisadaya <gisseldiaz176@gmail.com>
2026-09-09 20:24:48 -0600

    Agregando primeros datos de prueba de la tabla parcelas

--- database/02_datos_prueba.sql
+++ database/02_datos_prueba.sql
@@ -3,8 +3,8 @@
 INSERT INTO public."Parcelas"
     (id_parcela, id_usuario, nombre, tipo_cultivo)
 VALUES
-    (101, 1, 'Parcela Prueba Norte', 'Tomate'),
-    (102, 1, 'Parcela Prueba Sur', 'Chile'),
-    (103, 2, 'Parcela Prueba Este', 'Pepino'),
-    (104, 2, 'Parcela Prueba Oeste', 'Lechuga'),
-    (105, 3, 'Parcela Prueba Central', 'Maiz');
+    (4, 1, 'Huerto Principal', 'Lechuga'),
+    (5, 2, 'Huerto Secundario', 'Maiz'),
+    (6, 3, 'Huerto Terceario', 'Pepino'),
+    (7, 1, 'Huerto Principal', 'Zanahoria'),
+    (8, 2, 'Huerto Secundario', 'Frijol');
