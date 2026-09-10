7cd8359da387429d04b4a94bd46edc65d1dee6d6
Gisadaya <gisseldiaz176@gmail.com>
2026-09-09 20:11:51 -0600

    Agrega datos de prueba de parcelas

---
+++ database/02_datos_prueba.sql
@@ -0,0 +1,10 @@
+-- Datos de prueba para la tabla Parcelas
+
+INSERT INTO public."Parcelas"
+    (id_parcela, id_usuario, nombre, tipo_cultivo)
+VALUES
+    (101, 1, 'Parcela Prueba Norte', 'Tomate'),
+    (102, 1, 'Parcela Prueba Sur', 'Chile'),
+    (103, 2, 'Parcela Prueba Este', 'Pepino'),
+    (104, 2, 'Parcela Prueba Oeste', 'Lechuga'),
+    (105, 3, 'Parcela Prueba Central', 'Maiz');
